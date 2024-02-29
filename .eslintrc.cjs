763ff3092c3073f68be91a7c4f652cf7b7df4990
ShaoBin Deng <874977011@qq.com>
2024-02-29 19:01:37 +0800

    添加演示图片

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -12,10 +12,10 @@ module.exports = {
     ecmaVersion: 'latest'
   },
   rules: {
-    // prettier专注于代码的美观度（格式化工具）
+    // prettier专注于代码的美观度 (格式化工具)
     // 前置：
-    // 1.禁用格式化插件 prettier   format on save 关闭
-    // 2.安装Eslint插件,并配置保存时自动修复
+    // 1. 禁用格式化插件 prettier  format on save 关闭
+    // 2. 安装Eslint插件, 并配置保存时自动修复
     'prettier/prettier': [
       'warn',
       {
@@ -26,15 +26,20 @@ module.exports = {
         endOfLine: 'auto' // 换行符号不限制（win mac 不一致）
       }
     ],
-    // ESLint关注于规范，如果不符合规范，报错
+    // ESLint关注于规范, 如果不符合规范，报错
     'vue/multi-word-component-names': [
       'warn',
       {
         ignores: ['index'] // vue组件名称多单词组成（忽略index.vue）
       }
     ],
-    'vue/no-setup-props-destructure': ['off'], // 关闭 props 解构的校验（props解构丢失响应式）
-    // 💡 添加未定义变量错误提示，create-vue@3.6.3 关闭，这里加上是为了支持下一个章节演示。
+    'vue/no-setup-props-destructure': ['off'], // 关闭 props 解构的校验 (props解构丢失响应式)
+    // 添加未定义变量错误提示，create-vue@3.6.3 关闭，这里加上是为了支持下一个章节演示。
     'no-undef': 'error'
+  },
+  globals: {
+    ElMessage: 'readonly',
+    ElMessageBox: 'readonly',
+    ElLoading: 'readonly'
   }
 }
